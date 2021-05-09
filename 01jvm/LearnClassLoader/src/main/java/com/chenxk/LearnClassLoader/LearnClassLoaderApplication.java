9bb3b019b43dd6213178baf89e048139f37b740a
chenxk <1149367312@qq.com>
2021-05-09 18:48:47 +0800

    自定义cloassloader

---
+++ 01jvm/LearnClassLoader/src/main/java/com/chenxk/LearnClassLoader/LearnClassLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.chenxk.LearnClassLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnClassLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnClassLoaderApplication.class, args);
+	}
+
+}
---
+++ 01jvm/LearnClassLoader/src/test/java/com/chenxk/LearnClassLoader/LearnClassLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxk.LearnClassLoader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnClassLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
