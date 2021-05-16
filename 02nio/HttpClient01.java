31b56161f48062be7df1da573b3965891df88fd3
chenxk <1149367312@qq.com>
2021-05-16 14:49:01 +0800

    使用okhttp访问服务器 ’ ‘

---
+++ 02nio/HttpClient01.java
@@ -0,0 +1,36 @@
+package com.chenxk.LearnClassLoader;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * @author chenxiaokang
+ * @date 2021/5/16
+ */
+public class HttpClient01 {
+    private static OkHttpClient client = new OkHttpClient();
+    public static void main(String[] args) {
+        String url ="http://localhost:8801/";
+        try {
+            System.out.println(run(url));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private static String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+}
