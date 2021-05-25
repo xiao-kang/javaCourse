7b1d80720498aaf58288aac79dbdc1abcda0c516
chenxk <1149367312@qq.com>
2021-05-25 17:08:26 +0800

    week3

---
+++ 01jvm/LearnClassLoader/src/main/java/com/chenxk/LearnClassLoader/GetExample.java
@@ -0,0 +1,31 @@
+package com.chenxk.LearnClassLoader;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * @author chenxiaokang
+ * @date 2021/5/25
+ */
+public class GetExample {
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        GetExample example = new GetExample();
+        String response = example.run("https://trade.shanghaitrust.com/");
+        System.out.println(response);
+    }
+}
